225026f1a81b64717c7b14efe02208320c1d6b0b
Giu.Giancola <giusepe.giancola@unipegaso.it>
2022-06-28 12:06:58 +0200

    mm

--- prove-varie/script.js
+++ prove-varie/script.js
@@ -4,8 +4,7 @@ const ul = d3.select('#list').append('ul')
 
 ul.selectAll('li').data(dataset).join('li').text(d => d)
 
-
-const add = () => {
+/* const add = () => {
     console.log('add', dataset)
 
     dataset.push(dataset[dataset.length - 1] + 10)
@@ -19,8 +18,9 @@ const add = () => {
         )
         .text(d => d)
     console.log(newul)
-}
-const remove = () => {
+} */
+
+/* const remove = () => {
     console.log('remove')
 
     dataset.pop()
@@ -35,9 +35,25 @@ const remove = () => {
         .text(d => d)
     console.log(newul)
 
+} */
+
+const edit = (val) => {
+
+    val ? dataset.push(dataset[dataset.length - 1] + 10) : dataset.pop()
+
+    d3.select('ul')
+        .selectAll('li')
+        .data(dataset)
+        .join(
+            enter => enter.append('li').style('color', 'green'),
+            update => update.style('color', 'purple'),
+            exit => exit.remove()
+        )
+        .text(d => d)
+
 }
 
-d3.select('#add').on('click', add)
-d3.select('#remove').on('click', remove)
 
-
+d3.select('#add').on('click', () => edit(true))
+d3.select('#remove').on('click', () => edit(false))
+console.log('dio')
